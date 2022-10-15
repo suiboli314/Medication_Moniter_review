5473f832bac8f54d5747963b33554a22baf72761
ahleung2 <alvanguard128@gmail.com>
2022-10-14 19:54:44 -0700

    Combined refill and restock controller

--- Backend/controllers/update-controller.js
+++ Backend/controllers/update-controller.js
@@ -1,19 +1,26 @@
-// restock inventory when medication is low
+// refill order for patients
 const queryControllers = require("./query-controllers")
+const mongoUtil = require("../mongoUtil")
 const DUMMY_MEDICATION = queryControllers.DUMMY_MEDICATION
 
-const restockOrder = (req, res) => {
+const prescriptionOrder = (req, res) => {
     // Want to take in id or name, and quantity required
     const { type, value, quantity } = req.body
+    const database = mongoUtil.getDB()
 
     let medication
     // Need to figure out what if type == name (use else statement)
     if (type === "id") {
         // update stock quantity
         medication = DUMMY_MEDICATION.find((x) => x.id === value)
-        medication.stock = medication.stock + quantity
+        if (quantity > medication.stock) {
+            console.log("Not enough medication in stock!")
+        } else {
+            medication.stock = medication.stock - quantity
+        }
+        //medication.stock = medication.stock - quantity
     }
     res.json({ medication: medication })
 }
 
-exports.restockOrder = restockOrder
+exports.prescriptionOrder = prescriptionOrder

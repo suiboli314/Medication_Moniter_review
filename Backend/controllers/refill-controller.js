4a450e4279204fc52027c8fddf5defcd36f3e220
Yao Zhong <a26250@baobeiben.local>
2022-10-19 16:00:38 -0700

    refill.js modified to send correct req

--- Backend/controllers/refill-controller.js
+++ Backend/controllers/refill-controller.js
@@ -5,6 +5,7 @@ const mongoUtil = require("../mongoUtil")
 const prescriptionOrder = async (req, res) => {
     // Want to take in id or name, and quantity required
     const { type, value, quantity } = req.body
+    console.log(req.body)
     console.log(type, value, quantity)
     const database = mongoUtil.getDB()
     let query
@@ -20,30 +21,26 @@ const prescriptionOrder = async (req, res) => {
 
     // check if medication exists
     // try{
-        if (!medication) {
-            console.log("Can't find medication in database")
-            // throw new Error("Can't find medication in database")
-        } else if (quantity > medication.stock) {
-            console.log("Not enough medication in stock!")
-            // res.status(500).json({msg: "Not enough medication in stock!"})
-            // throw new Error("Not enough medication in stock!")
-        } else {
-            const update = {
-                $inc: {
-                    stock: -1 * quantity,
-                },
-            }
-            await medications.updateOne(query, update)
-            medication = await medications.findOne(query)
+    if (!medication) {
+        console.log("Can't find medication in database")
+        // throw new Error("Can't find medication in database")
+    } else if (quantity > medication.stock) {
+        console.log("Not enough medication in stock!")
+        // res.status(500).json({msg: "Not enough medication in stock!"})
+        // throw new Error("Not enough medication in stock!")
+    } else {
+        const update = {
+            $inc: {
+                stock: -1 * quantity,
+            },
         }
-        res.json({ medication: medication })
+        await medications.updateOne(query, update)
+        medication = await medications.findOne(query)
+    }
+    res.json({ medication: medication })
     // }catch(error){
     //     res.status(500).json({msg: error})
     // }
-    
-    
 }
 
 exports.prescriptionOrder = prescriptionOrder
-
-

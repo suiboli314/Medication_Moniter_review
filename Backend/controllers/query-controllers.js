2d9ee3d001f4a7b1fe5d1fdac69baf09186356e6
Yao Zhong <a26250@baobeiben.local>
2022-10-19 14:39:03 -0700

    render query result

--- Backend/controllers/query-controllers.js
+++ Backend/controllers/query-controllers.js
@@ -1,5 +1,7 @@
 const mongoUtil = require("../mongoUtil")
 
+let lastQuery
+
 const queryMedication = async (req, res) => {
     const type = req.query.type
     const value = req.query.value
@@ -13,35 +15,15 @@ const queryMedication = async (req, res) => {
 
     const medication = await database.collection("medications").findOne(query)
 
-    res.json({ medication: medication })
+    lastQuery = medication
+
+    res.redirect("/")
 }
 
-const DUMMY_MEDICATION = [
-    {
-        id: "9988776",
-        name: "pepto",
-        manufactor: "abc",
-        stock: 7,
-    },
-    {
-        id: "6655443",
-        name: "sinux",
-        manufactor: "def",
-        stock: 0,
-    },
-    {
-        id: "1234567",
-        name: "amoxicillin",
-        manufactor: "ghi",
-        stock: 5,
-    },
-    {
-        id: "7654321",
-        name: "aspirin",
-        manufactor: "xyz",
-        stock: 2,
-    },
-]
+const getLastQuery = (req, res) => {
+    console.log(lastQuery)
+    res.json({ lastQuery: lastQuery })
+}
 
 exports.queryMedication = queryMedication
-exports.DUMMY_MEDICATION = DUMMY_MEDICATION
+exports.getLastQuery = getLastQuery

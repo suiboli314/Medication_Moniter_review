cc9c695b2cc79ea5ab7549862bf64f193f77e725
Yao Zhong <a26250@baobeiben.local>
2022-10-31 17:15:28 -0700

    figureing deployment problem

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoUtil = require("./mongoUtil");
+const path = require("path");
 
 const app = express();
 const port = 3000;
@@ -21,7 +22,8 @@ mongoUtil.connectToClient();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static("./public"));
+// app.use(express.static("./public"));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.use("/query", queryRoutes);
 
